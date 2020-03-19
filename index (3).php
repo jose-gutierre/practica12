3ea2e284b6ac93ddd2ca3d94cd9157348aaba7ec
jose-gutierre <60245402+jose-gutierre@users.noreply.github.com>
2020-03-18 19:40:59 -0600

    practica12

---
+++ index (3).php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica12</title>
+</head>
+<body>
+<div>
+<h1>formula</h1>
+    <form name="formulario" method="post" action="procesar.php">
+    Nombre: <input type="text" name="nombre" value="">
+    Apellidos: <input type="text" name="apellidos" value="">
+    <input type="submit" value="enviar" />
+    </form>
+    </div>
+    <a href="area.php"> Calcular area del circulo</a>
+
+</body>
+</html>
